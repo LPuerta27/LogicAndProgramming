bfdd7a89f04f5b7f3ab82a138fb025b5aaa5cef9
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-03-27 15:30:18 -0500

    Nuevo programa ciclo for para hallar  el factorial de un numero

---
+++ CicloFor_Factorial/CicloFor_Factorial/CicloFor_Factorial/CicloFor_Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CicloFor_Factorial
+{
+    public class CicloFor_Factorial
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo 12: Desarrolle un algoritmo que pida por pantalla N números y calcule la factorial de ellos.
+
+            double n, i, factorial = 1;
+
+            Console.Write("Digite el número n (tope): ");
+            n= Convert.ToDouble(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+
+            }
+            Console.WriteLine("El factorial de: " + n + " es: " + factorial);
+
+        }
+    }
+}
